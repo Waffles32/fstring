eb031a5fa37fa67b21b4bb6c56e29ca6f24c0e9b
Waffles32 <>
2019-01-23 06:44:44 -0600

    oneliner

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cp fstring /usr/local/bin
 
-
+fstring='python -c "import sys,json;print(open(sys.argv[1]).read().format(**json.load(sys.stdin)))"'
 
 ```console
 $ sudo wget -O /usr/local/bin https://github.com/Waffles32/fstring/blob/master/fstring
